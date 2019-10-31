0fe274c6c0c300abae085c4d46a3d5cb3562cc37
Rinor Rafuna <rinor_rafuna@hotmail.com>
2019-10-31 10:14:29 +0100

    Doreta test

---
+++ src/main/java/tung/DoretaTest.java
@@ -0,0 +1,8 @@
+package tung;
+
+public class DoretaTest {
+ public static void main(String[] args){
+  Test1 test= new Test1();
+  System.out.println(test.ktheShumen());
+}
+}
---
+++ src/main/java/tung/Test1.java
@@ -0,0 +1,8 @@
+package tung;
+
+public class Test1{
+ public double ktheShumen(){
+  int a=4,b=5;
+   return a+b;
+}
+}
